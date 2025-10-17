8a5a2b117a5ccf672f6194f64e32282d47c9e1bb
yosvelastoponce-bit <yosvelastoponce@gmail.com>
2025-10-16 23:01:33 -0500

    Fucion con la base de datos

--- Dream/routes/web.php
+++ Dream/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Usuario;
+use Illuminate\Http\
 Route::get('/', function () {
+    $usuario = new Usuario();
+    $usuario->nombre = 'Juan Perez';
+    $usuario->email = 'juanperez@gmail.com';
+    $usuario->contraseña = 'bla123';
+    $usuario->save();
+
     return view('welcome');
 });
+
+Route::get('/usuario', function () {
+    return View('usuario.index', compact('usuario'));
+}); 
