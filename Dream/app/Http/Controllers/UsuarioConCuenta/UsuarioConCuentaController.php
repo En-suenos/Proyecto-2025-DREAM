ed1aeafa02be1c3542af3f4c436d7d83d78e54d4
yosvelastoponce-bit <yosvelastoponce@gmail.com>
2025-10-17 23:01:34 -0500

    commit 3

--- Dream/app/Http/Controllers/UsuarioConCuenta/UsuarioConCuentaController.php
+++ Dream/app/Http/Controllers/UsuarioConCuenta/UsuarioConCuentaController.php
@@ -6,13 +6,15 @@ use App\Http\Controllers\VentanaPrincipal\VentanaPrincipalController;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\{Redirect,View};
 use Illuminate\Http\Request;
+use App\Models\Usuario;
 
 class UsuarioConCuentaController extends Controller
 {
     // conectar con la vista
     public function index(Request $request)
     {
-        return view('usuarios con cuenta.index');
+        $usuariosConCuenta = Users::orderBy('id', 'DESC')->get();
+        return view('usuarios con cuenta.index', compact('usuariosConCuenta'));
     }
 
     public function find(Request $request)
--- Dream/routes/web.php
+++ Dream/routes/web.php
@@ -10,7 +10,8 @@ use App\Http\Controllers\Perfil\PerfilController;
 use App\Http\Controllers\UsuarioConCuenta\UsuarioConCuentaController;
 use App\Http\Controllers\Playlist\PlaylistController;
 use App\Http\Controllers\AsistenteIA\AsistenteIAController;
-//use Illuminate\Http\
+
+use Illuminate\Http\Request;
 
 /*
 Route::get('/', function () {
@@ -19,14 +20,24 @@ Route::get('/', function () {
 */
 
 Route::get('/', function () {
-    return view('usuarios con cuenta.index ');
+    $usuario1 = new Usuario();
+    $usuario1->nombre = 'Rosy';
+    $usuario1->email = 'rosy@gmail.com';
+    $usuario1->contrasena = '12345';
+    $usuario1->tipoUsuario = 'normal';
+    $usuario1->fechaRegistro = '2023-10-01';
+    $usuario1->save();
+
+    return $usuario1;
+    //return view('ventana principal.index', compact('usuario'));
+    //return view('usuarios con cuenta.index ');
 });
 /*
 Route::get('/usuario', function () {
     return View('usuario.index', compact('usuario'));
 }); 
 */
-
+/*
 //Se comunican con Controllers---------------------------------------------------------------------------------------
 //Ruta para la ventana principal
 Route::get('/ventana principal/index', [VentanaPrincipalController::class, 'index'])->name('ventana principal.index');
@@ -46,3 +57,4 @@ Route::get('/playlists/index', [PlaylistController::class, 'index'])->name('play
 
 //Ruta para Asistente IA
 Route::get('/asistente ia/index', [AsistenteIAController::class, 'index'])->name('asistente ia.index');
+*/
