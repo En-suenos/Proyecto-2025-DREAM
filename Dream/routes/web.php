78000062cbd21c4daa00a01939db3cc2cbabc5f6
yosvelastoponce-bit <yosvelastoponce@gmail.com>
2025-11-11 09:56:45 -0500

    Login

--- Dream/routes/web.php
+++ Dream/routes/web.php
@@ -21,15 +21,19 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('VentanaUsuarioCuenta/Index');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/Principal', function () {
+    return Inertia::render('VentanaUsuarioCuenta/Index');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('/ConCuenta', VentanaUsuarioConCuentaInertiaController::class);
 Route::resource('/perfil', PerfilInertiaController::class);
